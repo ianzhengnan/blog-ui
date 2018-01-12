fb4e09041e3a4fe8b4550a0311f01380935e1636
IanZheng <flks_2005@qq.com>
2018-01-13 00:23:47 +0800

    fix highlight

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,7 @@ const Layout = (props) => (
       <meta charSet='utf-8' />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous" />
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.10.0/styles/atom-one-light.min.css" rel="stylesheet" />
       <link rel="stylesheet" href="/static/css/main.css" />
     </Head>
     <Header />
